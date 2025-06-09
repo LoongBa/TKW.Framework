08993455d1e5d66d9f0a3dfe963bdd62509e3649
LoongBa <76543969+LoongBa@users.noreply.github.com>
2025-06-09 17:33:59 +0800

    更新数据加载器并添加新字典条目
    在 `DMP-Lite.sln.DotSettings` 中添加了 "DMPF" 用户字典条目。
    在 `DMP-Lite.Domain.csproj` 中添加了对 `TKWF.DMPCore.csproj` 的引用。
    移动 `DatabaseDataLoader` 类到领域项目（涉及数据库连接和具体业务）。
    新增 `ExcelDataLoader` 类，实现了 `IDataLoader` 接口，定义了 `SourceType` 属性，待有空时填充读取 Excel 部分代码。

---
+++ _Extensions/DMPCore/Plugins/DataLoaders/ExcelDataLoader.cs
@@ -0,0 +1,14 @@
+﻿using TKWF.DMPCore.Interfaces;
+using TKWF.DMPCore.Models;
+
+namespace TKWF.DMPCore.Plugins.DataLoaders
+{
+    internal class ExcelDataLoader: IDataLoader
+    {
+        public string SourceType { get; }
+        public IEnumerable<Dictionary<string, object>> Load(DataLoadOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
